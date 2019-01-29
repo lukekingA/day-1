e0a2ecf184041f0e41536375fda65cd8f9bbd497
Luke King <lkingfrm@gmail.com>
2019-01-29 00:16:57 -0700

    Rewrite to take inputs form a user

---
+++ scripts.js
@@ -0,0 +1,35 @@
+const yourName = document.getElementById("name");
+const greeting = document.getElementById("intro_p");
+const greetingValue = greeting.innerText;
+const history1 = document.getElementById("history1");
+const history2 = document.getElementById("history2");
+const inputs = Array.from(document.getElementsByTagName("input"));
+
+//handler function for input updates
+function updater(event) {
+  let text = event.target.value;
+  document.getElementById(event.target.id + "a").innerText = text;
+}
+
+inputs.forEach(item => {
+  if (item.id != "hobbies") {
+    item.addEventListener("keyup", updater)
+  } else {
+    item.addEventListener("keydown", (e) => {
+      if (e.keyCode === 13) {
+        document.getElementsByTagName("ul")[0].appendChild(document.createElement("li")).innerText = e.target.value;
+        e.target.value = "";
+      }
+    })
+  }
+})
+
+
+
+
+
+
+// yourName.addEventListener("keyup", (e) => {
+//   updater(e);
+//   greeting.innerText = greetingValue + " " + input.value;
+// })
