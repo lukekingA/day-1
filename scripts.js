423d68fb80c189a0dd44636f46df770672850948
Luke King <lkingfrm@gmail.com>
2019-01-29 08:51:11 -0700

    removed unused variables

--- scripts.js
+++ scripts.js
@@ -1,8 +1,3 @@
-const yourName = document.getElementById("name");
-const greeting = document.getElementById("intro_p");
-const greetingValue = greeting.innerText;
-const history1 = document.getElementById("history1");
-const history2 = document.getElementById("history2");
 const inputs = Array.from(document.getElementsByTagName("input"));
 
 //handler function for input updates
@@ -11,17 +6,18 @@ function updater(event) {
   document.getElementById(event.target.id + "a").innerText = text;
 }
 
-
 //Adding Handlers for the inputs
 inputs.forEach(item => {
   if (item.id != "hobbies") {
-    item.addEventListener("keyup", updater)
+    item.addEventListener("keyup", updater);
   } else {
-    item.addEventListener("keydown", (e) => {
+    item.addEventListener("keydown", e => {
       if (e.keyCode === 13) {
-        document.getElementsByTagName("ul")[0].appendChild(document.createElement("li")).innerText = e.target.value;
+        document
+          .getElementsByTagName("ul")[0]
+          .appendChild(document.createElement("li")).innerText = e.target.value;
         e.target.value = "";
       }
-    })
+    });
   }
-})
+});
